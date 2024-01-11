278078b77811a0a079f211349a69da13661478f4
Si Thu htet <sithu.tgi90@gmail.com>
2024-01-11 23:24:30 +0630

    first commit

---
+++ src/features/icecream/IceCreamView.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { ordered, restocked } from './iceCreamSlice'
+
+export const IceCreamView = () => {
+    const [value, setValue] = React.useState(1);
+    const numOfIceCreams = useSelector(state => state.iceCream.numberOfIceCreams)
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+            <h2>Number of ice creams - {numOfIceCreams}</h2>
+            <button onClick={() => dispatch(ordered())}>Order ice cream</button>
+            <input type="number" onChange={(e) => setValue(parseInt(e.target.value))} />
+            <button onClick={() => dispatch(restocked(value))}>Restock ice cream</button>
+        </div>
+    )
+}
